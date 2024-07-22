208379d31a16f041b09718883acf3c3f4859bb4a
AlexKat <katsubo79@gmail.com>
2024-07-22 19:10:49 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -58,7 +58,7 @@ const questions = [
   },
   {
     question: 'Выберите правильную фигуру из четырёх пронумерованных.',
-    picture: '../assets/img/image1.jpg',
+    picture: './public/image.png',
     answers: ['1', '2', '3', '4 '],
     shablon: '3',
   },
@@ -74,7 +74,7 @@ const questions = [
   {
     question:
       'Какое определение, по-Вашему, больше подходит к этому геометрическому изображению:',
-    picture: '../assets/img/image2.jpg',
+    picture: './public/image-.png',
     answers: [
       'Оно остроконечное',
       'Оно устойчиво',
@@ -84,7 +84,7 @@ const questions = [
   },
   {
     question: 'Вставьте подходящее число',
-    picture: '../assets/img/image3.jpg',
+    picture: './public/image--.png',
     answers: ['34', '36', '53', '44', '66', '42'],
     shablon: '3',
   },
