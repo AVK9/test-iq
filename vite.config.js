a5aa3247b915e17723a1b94587c75225d9301455
AlexKat <katsubo79@gmail.com>
2024-07-23 10:43:45 +0300

    base

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
+    base: '/test-iq/',
     root: 'src',
     build: {
       sourcemap: true,
